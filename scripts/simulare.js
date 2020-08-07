17c8963dd7b302cfbf228b5309a2d3c373c68474
Euphonic Design <25894355+euphonicdesign@users.noreply.github.com>
2020-08-07 21:29:38 +0300

    c305

--- scripts/simulare.js
+++ scripts/simulare.js
@@ -1683,7 +1683,7 @@ function desenareGraficVertical(){
     ctx.font = "italic 16px Helvetica, system-ui, Arial, sans-serif";
     ctx.fillStyle = culoareTextCompensatorFill;
     ctx.strokeStyle = culoareTextCompensatorFill;
-    ctx.fillText("+" + Math.round(proiectie), x_val_1, y_val_2 - 4);
+    ctx.fillText("+" + Math.round(proiectie) + "!", x_val_1, y_val_2 - 4);
 
     for (let i = 0; i <= selectorZi; i++) {
 
