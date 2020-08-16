a02e56fc4d5dfd1d235c39452433999eaf68b721
Euphonic Design <25894355+euphonicdesign@users.noreply.github.com>
2020-08-16 23:41:34 +0300

    c376

--- scripts/simulare.js
+++ scripts/simulare.js
@@ -172,7 +172,7 @@ let evenimente_actiuni = [
               '2.set masuri relaxare?', //1/6
               '3.set masuri relaxare??', //15/6
               '4.set masuri relaxare???', //1/7
-              '5.set masuri preventie', //1/8
+              '5.set masuri preventie...', //1/8
 ];
 
 var xTextZi = 10;
