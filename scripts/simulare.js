026900112e2d036b82e1a681a93dac87771278c6
Euphonic Design <25894355+euphonicdesign@users.noreply.github.com>
2020-09-10 14:19:24 +0300

    c701

--- scripts/simulare.js
+++ scripts/simulare.js
@@ -3349,13 +3349,13 @@ function desenareGraficOrizontal(){
     //ctx.fillText("Ziua " + (Math.floor(selectorZi) + intervalProiectie2), x_val_2 + 8, y_val_1 + 40);
 
     //ctx.font = "italic 14px Helvetica, system-ui, Arial, sans-serif";
-    ctx.fillText(zi_p + " " + luna_p, x_val_2 + 18, y_val_1 + 40);
+    ctx.fillText(zi_p + " " + luna_p, x_val_2 + 26, y_val_1 + 44);
 
     ctx.textAlign = "start";
     //ctx.strokeStyle = culoareTextCompensatorRosu;//culoareTextCompensatorFill;
     ctx.fillStyle = culoarePunctValoriGrafic;
     ctx.font = "italic bold 16px Helvetica, system-ui, Arial, sans-serif";
-    ctx.fillText(">" + Math.round(dataCumulativ[selectorZi] + proiectie2) + "!", x_val_2 + 18, y_val_1 + 54);
+    ctx.fillText(">" + Math.round(dataCumulativ[selectorZi] + proiectie2) + "!", x_val_2 + 26, y_val_1 + 61);
     //ctx.fillStyle = culoarePunctValoriGrafic_3;
     //ctx.fillText("" + Math.round(dataCumulativ[selectorZi]), 10, y_val_1);
 
