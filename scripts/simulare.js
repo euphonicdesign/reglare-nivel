28413bbc9b397bca3bff77e2e2c1af36480bf4ea
Euphonic Design <25894355+euphonicdesign@users.noreply.github.com>
2020-12-03 09:57:04 +0200

    c179

--- scripts/simulare.js
+++ scripts/simulare.js
@@ -296,7 +296,7 @@ var scalareTraiectorieAvion = 2;
 
 
 //MAGNIFICARE RADAR IMPLICITA
-var scalaRadarY = 7.5; //magnificare radar implicita
+var scalaRadarY = 7.0; //magnificare radar implicita
 var scalaRadarX = 1;
 
 var scalaX_trend = 72;
